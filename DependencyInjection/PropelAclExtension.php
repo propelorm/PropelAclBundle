71f7db0624366c6603604a10f0214780821efb16
Toni Uebernickel <tuebernickel@gmail.com>
2016-01-28 17:05:53 +0100

    add PropelAclExtension loading services

---
+++ DependencyInjection/PropelAclExtension.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Propel\Bundle\PropelAclBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+final class PropelAclExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
+    }
+}
